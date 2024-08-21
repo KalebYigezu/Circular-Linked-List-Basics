f3586aab84f48651abe890b7e7915d682e265723
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-08-20 18:51:41 -0700

    Update CircularSingleLinkedList.py

--- CircularSingleLinkedList.py
+++ CircularSingleLinkedList.py
@@ -1,1 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class CLL:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def display(self):
+        if self.head is None:
+            print("Empty Circular Linked List")
+        else:
+            temp = self.head
+            print(temp.data,"-->", end=" ")
+            while temp.next != self.head:
+                temp = temp.next
+                print(temp.data,"-->", end=" ")
+            print(temp.next.data)
+
+
+l = CLL()
+n1 = Node(10)
+l.head = n1
+l.tail = n1
+l.tail.next = l.head
+print("After first Node")
+l.display()
+
+n2 = Node(20)
+l.tail.next = n2
+l.tail = n2
+l.tail.next = l.head
+print("\nAfter second node")
+l.display()
+
+n3 = Node(30)
+l.tail.next = n3
+l.tail = n3
+l.tail.next = l.head
+print("\nAfter third node")
+l.display()
+
+
 
